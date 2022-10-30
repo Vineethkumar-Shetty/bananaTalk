42d1a151074e408945b5ff1b6024e27758d15632
Vineethkumar-Shetty <shettyvineethkumar@gmail.com>
2022-10-31 01:52:27 +0530

    style: formatted the structure

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var inputText = document.querySelector('#inputArea');
+var inputText = document.querySelector("#inputArea");
 var button = document.querySelector("#btn-translate");
-var outputText = document.querySelector('#outputArea');
-var url = 'https://api.funtranslations.com/translate/minion.json';
+var outputText = document.querySelector("#outputArea");
+var url = "https://api.funtranslations.com/translate/minion.json";
 
-function getServerURL(text){
-    return encodeURI(url + "?text=" + text);
+function getServerURL(text) {
+  return encodeURI(url + "?text=" + text);
 }
 
-function translateToMinion () {
-    fetch(getServerURL(inputText.value)).then(result => result.json()).then(json => {
-        var output = json.contents.translated;
-        outputText.innerText = output;
+function translateToMinion() {
+  fetch(getServerURL(inputText.value))
+    .then((result) => result.json())
+    .then((json) => {
+      var output = json.contents.translated;
+      outputText.innerText = output;
     });
-};
-button.addEventListener('click', translateToMinion);
+}
+button.addEventListener("click", translateToMinion);
