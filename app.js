a4f87eb279eae6d9ed1f34f19f53c260d329c42a
Vineethkumar-Shetty <shettyvineethkumar@gmail.com>
2022-10-31 01:40:59 +0530

    feat: updated the url and styles

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-area");
-var txtOutput = document.querySelector("#output-text");
-var serverURL = "https://jsonplaceholder.typicode.com/todos/1";
-function getTranslationUrl (text) {
-    return serverURL + "?"+text;
-}  
-console.log(btnTranslate);
-function clickHandler() {
-//   console.log("ayyyoo...Rama");
-//   console.log(txtInput.value);
-//   txtOutput.innerText = "cbbchjbdchj " + txtInput.value;
-var inputText = txtInput.value;
-fetch(getTranslationUrl(inputText)).then(response=>response.json()).then(json=>{var outvalue =json.title;
-txtOutput.innerText = outvalue})
+var inputText = document.querySelector('#inputArea');
+var button = document.querySelector("#btn-translate");
+var outputText = document.querySelector('#outputArea');
+var url = 'https://api.funtranslations.com/translate/minion.json';
 
+function getServerURL(text){
+    return encodeURI(url + "?text=" + text);
 }
-btnTranslate.addEventListener("click", clickHandler);
+
+function translateToMinion () {
+    fetch(getServerURL(inputText.value)).then(result => result.json()).then(json => {
+        var output = json.contents.translated;
+        outputText.innerText = output;
+    });
+};
+button.addEventListener('click', translateToMinion);
